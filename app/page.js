d6e7d31d2567f94bbec3fec0f79b05916b2a4a95
Gayathri Girish Nair <ggnair2000@gmail.com>
2024-05-16 15:34:15 +0100

    basic set up complete

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 const Home = () => {
-  return (
-    <main className="
-        h-screen w-full p-5 
-        grid grid-cols-10 grid-rows-9 gap-2
-    "></main>
-  )
+    // console.log(process.env.NEXT_PUBLIC_TEST)
+    return (
+        <main className="
+            h-screen w-full p-5 
+            grid grid-cols-10 grid-rows-9 gap-2
+        ">
+
+        </main>
+    )
 }
 
 export default Home

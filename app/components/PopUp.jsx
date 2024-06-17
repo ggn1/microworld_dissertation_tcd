f36009e8952a637946405e8d96d23e67b27e62c5
Gayathri Girish Nair <ggnair2000@gmail.com>
2024-06-17 16:14:01 +0100

    fixed overlay

--- app/components/PopUp.jsx
+++ app/components/PopUp.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+'use client'
+
+import {useEffect} from 'react'
 
 const PopUp = ({children, handleClose}) => {
     /**
      * A popup window.
      */
+
+    const onClose = () => {
+        document.body.classList.remove('overflow-hidden')
+        handleClose()
+    }
+
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden')
+    }, [])
+
     return (
         <div className='
-            absolute z-20 top-0 left-0 w-screen h-full
+            absolute z-20 top-0 left-0 w-screen h-screen
             flex justify-center *:select-none
         '>
             <div 
                 className='absolute w-full h-full bg-black opacity-80'
-                onClick={handleClose}
+                onClick={onClose}
             ></div>
             <div className='
                 absolute grid place-items-center bg-white rounded-xl 
-                opacity-100 m-10 p-10 w-3/4 max-h-96 overflow-scroll
+                opacity-100 m-10 p-10 w-3/4 max-h-[90%] overflow-scroll
             '>
                 <div className='flex flex-col gap-6 justify-center'>
                     {children}
